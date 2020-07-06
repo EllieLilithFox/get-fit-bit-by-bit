9dc756bfaecec7fd03ebf16230c639065f360843
Ian Scott <chesnekov@gmail.com>
2020-07-06 14:58:33 -0700

    update index.html with template website. Adds new packet for managing image files, imports css files into main.js
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import './../css/font-awesome.min.css';
+import './../css/aos.css';
+import './../css/tooplate-gymso-style.css';
+// import './../css/styles.css';
+import $ from "jquery";
+
+
+/* <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/aos.css"></link> */
+
+$(document).ready(function() {
+
+});
