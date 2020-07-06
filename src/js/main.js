4ffcbc95ea4c65f84fd9f2bc88043082987e7c41
Ian Scott <chesnekov@gmail.com>
2020-07-06 16:20:24 -0700

    updates webpack.config.js with accurate entry-point address, adds beginning of frontend logic to main.js
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 import 'bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-//import './../css/font-awesome.min.css';
-import './../css/aos.css';
-import './../css/tooplate-gymso-style.css';
+// import './css/font-awesome.min.css';
+import '../css/aos.css';
+import '../css/tooplate-gymso-style.css';
 // import './../css/styles.css';
 import $ from "jquery";
 
@@ -12,15 +12,32 @@ import $ from "jquery";
     <link rel="stylesheet" href="css/aos.css"></link> */
 
 $(document).ready(function() {
+  $(".membership-form webform").submit(function(event) {
+    event.preventDefault();
+    let nameInput = $("#name").val();
+    let weightInput = parseInt($("#weight").val());
+    let sexInput = $("input:radio[name=sex]checked").val()
+  })
 
-});
+  $("#strength-exercise-form").submit(function(event) {
+    event.preventDefault();
+    let strengthType = $("#strenght-type").val();
+    let reps = parseInt(("#reps").val());
+    let sets = parseInt(("#sets").val());
+    let strenghtDate = $("")
+
+  })
+
 $("input[name$='bn']").click(function(event){
     var radio_value = $(this).val();
     if(radio_value=='1') {
-      $("#multibn").show("slow");
+        $("#multibn").show("slow");
     }
     else if(radio_value=='2') {
-      $("#multibn").hide("slow");
+        $("#multibn").hide("slow");
     }
-    })
-    $('[name="bn"]:checked').trigger('click');
+})
+$('[name="bn"]:checked').trigger('click');
+
+
+});
