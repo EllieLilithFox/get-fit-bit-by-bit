047b9a904be9b244895ee2ab0a32a5a0305657d0
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-06 11:48:50 -0700

    add class files and tests, add code for strength and aerobic classes
    co-authored by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

---
+++ src/aerobic-class.js
@@ -0,0 +1,10 @@
+export class Aerobic {
+  //name-type of exercise, time - in minutes, distance - in miles
+  constructor(name, time, distance) {
+    this.name = name;
+    this.time = time;
+    this.distance = distance;
+    this.date = new Date();
+  }
+}
+
---
+++ src/strength-class.js
@@ -0,0 +1,8 @@
+export class Strength {
+  constructor(name, sets, reps){
+    this.name = name;
+    this.sets = sets;
+    this.reps = reps;
+    this.date = new Date();
+  }
+}
