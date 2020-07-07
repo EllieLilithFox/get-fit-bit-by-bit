8a4fd4ad5ffa2fb619e6ed64509a7c8b520cc31d
Ian Scott <chesnekov@gmail.com>
2020-07-07 11:12:23 -0700

    start recreating index.html from the ground up with references to the template. includes updated styles.css, and removes font references. Working to minimize template influence which was causing build errors.
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,40 @@
 import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/font-awesome.min.css';
-import '../css/aos.css';
-import '../css/tooplate-gymso-style.css';
-// import './../css/styles.css';
+import './../css/styles.css';
 import $ from "jquery";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-/* <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/aos.css"></link> */
+// Maybe won't use these
+// import './css/font-awesome.min.css';
+//import '../css/aos.css';
+//import '../css/tooplate-gymso-style.css';
 
 $(document).ready(function() {
-  $(".membership-form webform").submit(function(event) {
-    event.preventDefault();
-    let nameInput = $("#name").val();
-    let weightInput = parseInt($("#weight").val());
-    let sexInput = $("input:radio[name=sex]checked").val()
-  })
+//   $(".membership-form webform").submit(function(event) {
+//     event.preventDefault();
+//     let nameInput = $("#name").val();
+//     let weightInput = parseInt($("#weight").val());
+//     let sexInput = $("input:radio[name=sex]checked").val()
+//   })
 
-  $("#strength-exercise-form").submit(function(event) {
-    event.preventDefault();
-    let strengthType = $("#strenght-type").val();
-    let reps = parseInt(("#reps").val());
-    let sets = parseInt(("#sets").val());
-    let strenghtDate = $("")
+//   $("#strength-exercise-form").submit(function(event) {
+//     event.preventDefault();
+//     let strengthType = $("#strenght-type").val();
+//     let reps = parseInt(("#reps").val());
+//     let sets = parseInt(("#sets").val());
+//     let strenghtDate = $("")
 
-  })
+//   })
 
-$("input[name$='bn']").click(function(event){
-    var radio_value = $(this).val();
-    if(radio_value=='1') {
-        $("#multibn").show("slow");
-    }
-    else if(radio_value=='2') {
-        $("#multibn").hide("slow");
-    }
-})
-$('[name="bn"]:checked').trigger('click');
+// $("input[name$='bn']").click(function(event){
+//     var radio_value = $(this).val();
+//     if(radio_value=='1') {
+//         $("#multibn").show("slow");
+//     }
+//     else if(radio_value=='2') {
+//         $("#multibn").hide("slow");
+//     }
+// })
+// $('[name="bn"]:checked').trigger('click');
 
 
 });
