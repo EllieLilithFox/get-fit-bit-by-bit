db49983c75fd7f0df95027b34a56517f5b7dc230
Ian Scott <chesnekov@gmail.com>
2020-07-08 09:50:04 -0700

    adds back-end logic update to integrate various classes, updates method calls to push new exercise objects to exercise array lists
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>
    Co-authored-by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- src/js/main.js
+++ src/js/main.js
@@ -13,75 +13,22 @@ import {StrengthExercise} from './strength-exercise';
 //import '../css/aos.css';
 //import '../css/tooplate-gymso-style.css';
 
-/*let today = new Date(); 
-function getFullDate(date) {
-  return `${date.getDate()} ${date.getMonth() + 1} ${date.getFullYear()}`; //"7 7 2020"
-}
-let newDayActivity;
-
-function createNewDateObject() {
-  if (getFullDate(today) == newDayActivity.date) {
-    return newDayActivity;
-  } else {
-    newDayActivity = new DayActivity(getFullDate(today));
-  }
+/*function getFullDate(date) {
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`; //"7 7 2020"
 }*/
-
-
-// Need a function to create a new date object if one doesn't already exist,
-// If one does, then push Aerobic/strength exercise object to the appropriate date's exercise array
-
-function makeOrPushExerciseObjectAndDateObject (person, date, exerciseType, exerciseObject) {
-  for(const dateObject of person.dateObjects) {
-    if (dateObject.date === date) {
-      if(exerciseType === "Aerobic") {
-        // push AerobicExercise to aerobicExerciseArray
-        //break
-      } else if (exerciseType === "Strength") {
-        // push StrengthExercise to strengthExerciseArray
-        //break
-      }
-    }
-      // make a new dateObject and push to person, with exercise pushed to the new dateObject exercise array
-      if(exerciseType === "Aerobic") {
-        // push AerobicExercise to aerobicExerciseArray
-        // break
-      } else if (exerciseType === "Strength"{
-        // push StrengthExercise to strengthExerciseArray
-        // break
-      }
-    }
-}
 
 $(document).ready(function() {
   let person;
   let strengthExercise;
-  let aerobicExercise;
-  
-  let today = new Date(); 
-  function getFullDate(date) {
-    return `${date.getDate()} ${date.getMonth() + 1} ${date.getFullYear()}`; //"7 7 2020"
-  }
-  let newDayActivity = new DayActivity(getFullDate(today));
+  let aerobicExercise; 
+  let newDayActivity;
 
-  /*function createNewDateObject() {
-    if (getFullDate(today) == newDayActivity.date) {
-      return newDayActivity;
-    } else {
-      newDayActivity = new DayActivity(getFullDate(today));
-    }
-  }*/
-
-  // Array of dates and their prospective exercises
-  //let exerciseDates = [];
-
-  $(".membership-form webform").submit(function(event) {
+  $(".membership-form").submit(function(event) {
     event.preventDefault();
     let nameInput = $("#name").val();
     let weightInput = parseInt($("#weight").val());
-    //let sexInput = $("input:radio[name=sex]checked").val()
     person = new Person (nameInput, weightInput);
-    $('#name').text(person.name);
+    console.log(person);
   });
 
   $("#strength-exercise-form").submit(function(event) {
@@ -89,11 +36,18 @@ $(document).ready(function() {
     let strengthType = $("#strength-type").val();
     let reps = parseInt($("#reps").val());
     let sets = parseInt($("#sets").val());
+    //make date input required or default !?!?!??!
+    let date = $("#strength-exercise-date").val();
     strengthExercise = new StrengthExercise(strengthType, sets, reps);
-    newDayActivity.addStrengthActivity(strengthExercise);
-    //console.log("Calories burned: " + newDayActivity.calcDayCalories(person));
-    //let strenghtDate = $("#strength-exercise-date")
-    //let date = new Date
+    if(!person.findDayActivityObject(date)) {
+      newDayActivity = new DayActivity(date);
+      person.addDayActivity(newDayActivity);
+      newDayActivity.addStrengthActivity(strengthExercise);
+    } else {
+      person.findDayActivityObject(date).addStrengthActivity(strengthExercise);
+    }
+    console.log(newDayActivity);
+    console.log(person);
   });
 
   $("#aerobic-exercise-form").submit(function(event) {
@@ -102,9 +56,17 @@ $(document).ready(function() {
     let time = parseInt($("#time").val());
     let distance = parseInt($("#distance").val());
     let intensity = $("#intensity").val();
+    let date = $("#aerobic-exercise-date").val();
     aerobicExercise = new AerobicExercise(aerobicType, time, distance, intensity);
-    console.log("Intensity coef: " + aerobicExercise.calculateIntensityCoef());
-    newDayActivity.addAerobicActivity(aerobicExercise);
+    if(!person.findDayActivityObject(date)) {
+      newDayActivity = new DayActivity(date);
+      person.addDayActivity(newDayActivity);
+      newDayActivity.addAerobicActivity(aerobicExercise);
+    } else {
+      person.findDayActivityObject(date).addAerobicActivity(aerobicExercise);
+    }
+    console.log(newDayActivity);
+    console.log(person);
   });
 
   $("input[name$='exercise-type']").click(function(){
--- src/js/person-class.js
+++ src/js/person-class.js
@@ -2,6 +2,22 @@ export class Person {
   constructor(name, weight) {
     this.name = name;
     this.weight = weight;
-    this.dateObjects = [];
+    this.dayActivityObjects = [];
+  }
+
+  addDayActivity(dayActivityObject) {
+    this.dayActivityObjects.push(dayActivityObject);
+  }
+
+  findDayActivityObject(date) {
+    if(this.dayActivityObjects.length > 0) {
+      for(let i=0; i < this.dayActivityObjects.length; i++) {
+        if(this.dayActivityObjects[i].date == date) {
+          return this.dayActivityObjects[i];
+        }
+      }
+    } else {
+      return false;
+    }    
   }
 }
